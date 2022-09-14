8449b31a728539e4bba7a56d33c44a547f037ce9
admin <admin@BCS-Ubuntu-32.lrccd.ad.losrios.edu>
2022-09-14 11:10:25 -0700

    https://github.com/PallyBae/Practice-Triangle-Game

--- Code/ChaosGame.cpp
+++ Code/ChaosGame.cpp
@@ -1,9 +1,103 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << "\n";
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game
+
+	RenderWindow window(vm, "Sierpinski Triangle!!", Style::Default);
+
+    vector<Vector2f> vertices;   ///push_back stuff into us!
+	vector<Vector2f> points;
+
+	Vector2f clicked;
+
+    while (window.isOpen())
+	{
+        Event event;
+        while (window.pollEvent(event))
+		{
+            if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					std::cout << "the left button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+
+                    if(vertices.size() < 3)
+                    {
+                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    }
+                    else if(points.size() == 0)
+                    {
+                        ///fourthclick
+                        ///push back to points vector
+                        ///maybe unnecessary
+                    }
+                    clicked.x = event.mouseButton.x;
+					clicked.y = event.mouseButton.y;
+
+				}
+			}
+    
+		}
+
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+
+        /*
+        ****************************************
+        Update the scene
+        ****************************************
+        */
+        ///Vector2f v(2,2);
+        ///RectangleShape rect(v);
+        RectangleShape rect(Vector2f(5,5));
+        rect.setPosition(clicked.x, clicked.y);
+        rect.setFillColor(Color::Blue);
+        
+        if(points.size() > 0)
+        {
+            ///generate point
+            ///select random vertex
+            ///calculate midpoint between vertex and last point in vector
+            ///pushback newly generated coordinate
+        }
+        
+        /*
+        ****************************************
+        Draw the scene
+        ****************************************
+        */
+
+        ///loop through vectors and draw each coordinate
+        // Clear everything from the last run frame
+        window.clear();
+        // Draw our game scene here
+        for(int i = 0; i < vertices.size(); i++)
+        {
+            RectangleShape rect(Vector2f(10,10));
+            rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+            rect.setFillColor(Color::Blue);
+            window.draw(rect);
+        }
+        
+        window.display();
+    }
 
     return 0;
 }
