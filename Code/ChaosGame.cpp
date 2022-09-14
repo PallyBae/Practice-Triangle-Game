c86da2a92337d533ec8b893737e45f76169bffa6
admin <admin@BCS-Ubuntu-24.lrccd.ad.losrios.edu>
2022-09-14 16:18:45 -0700

    http://github.com/PallyBae/Practice-Triangle-Game

--- Code/ChaosGame.cpp
+++ Code/ChaosGame.cpp
@@ -2,6 +2,7 @@
 using namespace sf;
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -29,27 +30,32 @@ int main()
 			}
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (event.mouseButton.button == sf::Mouse::Left)
+				if (event.mouseButton.button == sf::Mouse::Left && points.size() == 0)
 				{
 					std::cout << "the left button was pressed" << std::endl;
 					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
 					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-                    if(vertices.size() < 3)
+                    if(vertices.size() < 20)
                     {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
-                    else if(points.size() == 0)
-                    {
-                        ///fourthclick
-                        ///push back to points vector
-                        points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-                        ///maybe unnecessary
-                    }
+                    
                     clicked.x = event.mouseButton.x;
 					clicked.y = event.mouseButton.y;
 
 				}
+                else if (event.mouseButton.button == sf::Mouse::Right && vertices.size() >= 3)
+                {
+                    std::cout << "the right button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+
+                    points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+
+                    clicked.x = event.mouseButton.x;
+					clicked.y = event.mouseButton.y;
+                }
 			}
     
 		}
@@ -71,16 +77,32 @@ int main()
         
         if(points.size() > 0)
         {
+            const double PI = 3.141592653589793;
             int midpoint_x;
             int midpoint_y;
             int vert_select;
+            double n = vertices.size();
+            int sumtop = n/4;
+            double cosine_k;
+            double sum_total = 0;
 
-            for(int i = 0; i < 100; i++)
+            for(double k = 1; k < (sumtop + 1); k++)
+            {
+                cosine_k = cos((2*PI*k)/n);
+                sum_total += cosine_k;
+            }
+            
+            
+
+            double scale_factor = 1/( 2*(1 + sum_total));
+            
+
+            for(int i = 0; i < 200; i++)
             {
                 srand(time(0) + i);
-                vert_select = (rand() % 3);
-                midpoint_x = (vertices[vert_select].x + points[points.size()-1].x)/2;
-                midpoint_y = (vertices[vert_select].y + points[points.size()-1].y)/2;
+                vert_select = (rand() % vertices.size());
+                midpoint_x = (vertices[vert_select].x + points[points.size()-1].x)*scale_factor;
+                midpoint_y = (vertices[vert_select].y + points[points.size()-1].y)*scale_factor;
                 points.push_back(Vector2f(midpoint_x, midpoint_y));
             }
             
@@ -103,14 +125,14 @@ int main()
         // Draw our game scene here
         for(int i = 0; i < vertices.size(); i++)
         {
-            RectangleShape rect(Vector2f(1,1));
+            RectangleShape rect(Vector2f(3,3));
             rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
             rect.setFillColor(Color::Red);
             window.draw(rect);
         }
         for(int i = 1; i < points.size(); i++)
         {
-            RectangleShape rectp(Vector2f(1,1));
+            RectangleShape rectp(Vector2f(3,3));
             rectp.setPosition(Vector2f(points[i].x, points[i].y));
             rectp.setFillColor(Color::Blue);
             window.draw(rectp);
