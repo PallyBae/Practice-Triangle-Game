6522dc36361d729596d3be8a4a859d264b146fb8
admin <admin@BCS-Ubuntu-11.lrccd.ad.losrios.edu>
2022-09-16 16:57:43 -0700

    https://github.com/PallyBae/Practice-Triangle-Game

--- Code/ChaosGame.cpp
+++ Code/ChaosGame.cpp
@@ -12,16 +12,11 @@ int main()
 	VideoMode vm(1920, 1080);
 
 	// Create and open a window for the game
-
 	RenderWindow window(vm, "Sierpinski Triangle!!", Style::Default);
 
     vector<Vector2f> vertices;   ///push_back stuff into us!
 	vector<Vector2f> points;
 
-    int poly_mid_x;
-    int poly_mid_y;
-    int false_poly_mid_x = 0;
-    int false_poly_mid_y = 0;
 
 	Vector2f clicked;
 
@@ -32,43 +27,56 @@ int main()
     //Configuring the instruction text
     Text instruction_text;
     instruction_text.setFont(font);
-    instruction_text.setString("LEFT CLICK to place vertices\nRIGHT CLICK to START");
+    instruction_text.setString("LEFT CLICK to place vertices\nRIGHT CLICK to begin generating points");
     instruction_text.setCharacterSize(50);
     instruction_text.setFillColor(Color::White);
 
-    //Configuring the point counter
+    //Configuring the point counter text
     Text point_count_text;
     point_count_text.setFont(font);
     point_count_text.setString("Points = 0");
     point_count_text.setCharacterSize(75);
     point_count_text.setFillColor(Color::Green);
 
-    //Positioning The Instruction Text
+    //Positioning the instruction text
     FloatRect textRect = instruction_text.getLocalBounds();
     instruction_text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
     instruction_text.setPosition(1920/2.0f, 1080/2.0f);
 
-    //Positioning The Point Counter
+    //Positioning the point counter text
     point_count_text.setPosition(30, 30);
 
-
+    //Game Loop
     while (window.isOpen())
 	{
+
+        //Declaring some variables outside the point generation loop
+        //This is done incase we want to apply the midpoint translation at the end
+        //Rather than applying it when the points are being created
+        int poly_mid_x;
+        int poly_mid_y;
+        int false_poly_mid_x = 0;
+        int false_poly_mid_y = 0;
+
         Event event;
         while (window.pollEvent(event))
 		{
+            //Closing the window
             if (event.type == sf::Event::Closed)
 			{
 				window.close();
 			}
+            //Grabbing mouse button inputs for various commands
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
 				if (event.mouseButton.button == sf::Mouse::Left && points.size() == 0)
 				{
+                    //outputting coordinates to the consol for debugging purposes
 					std::cout << "the left button was pressed" << std::endl;
 					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
 					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-
+                    
+                    //Collecting vertices input coordinates up to a certain ammount
                     if(vertices.size() < 20)
                     {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
@@ -78,12 +86,16 @@ int main()
 					clicked.y = event.mouseButton.y;
 
 				}
+                //Using the right mouse button to place the first point and start the point generation
+                //This allows more than 3 vertices to be set
                 else if (event.mouseButton.button == sf::Mouse::Right && vertices.size() >= 3)
                 {
+                    //outputting coordinates to the consol for debugging purposes
                     std::cout << "the right button was pressed" << std::endl;
 					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
 					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
+                    //Adding the input coordinates to the points vector so it can later be drawn
                     points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
 
                     clicked.x = event.mouseButton.x;
@@ -93,6 +105,8 @@ int main()
     
 		}
 
+
+        //Giving an alternative way of closing the program
         if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
@@ -107,31 +121,41 @@ int main()
         ///Vector2f v(2,2);
         ///RectangleShape rect(v);
         
-        
+        //Starting the program only when the player adds the first point
         if(points.size() > 0)
         {
+            //Declaring necessary variables and constants for future calculations
+
+            //Used in calculating the scale factor
             const double PI = 3.141592653589793;
-            int midpoint_x;
-            int midpoint_y;
-            int vert_select;
             double n = vertices.size();
-            int sumtop = n/4;
             double cosine_k;
+            int sumtop = n/4; //Uses integer division similar to the floor function
             double sum_total = 0;
+            double scale_factor;
+
+            //Used for point generation
+            int vert_select;
+            int midpoint_x; //Only actually the midpoint when making a triangle
+            int midpoint_y; //Only actually the midpoint when making a triangle
+
+            //Used for weird math equation translation calculation
+            int translation_x = 0;
+            int translation_y = 0;
+            int poly_rad = 0;
+
+            //Used for the approximate midpoint translation calculation referenced above
             int largest_x = 0;
             int smallest_x = 0;
             int largest_y = 0;
             int smallest_y = 0;
-            int translation_x = 0;
-            int translation_y = 0;
-            int poly_rad = 0;
             int false_largest_x = 0;
             int false_largest_y = 0;
             int false_smallest_x = 0;
             int false_smallest_y = 0;
             
 
-            //Find the Midpoint of the Polygon
+            //Find the approximate Midpoint of the Polygon
             for (int i = 0; i < vertices.size(); i ++)
             {
                 //Find the top of the polygon
@@ -164,7 +188,7 @@ int main()
                 poly_mid_x = (largest_x + smallest_x)/2;
             }
 
-            //Find the number of terms to add in the sum
+            //Summation loop to calculate the sum portion of the scale factor
             for(double k = 1; k < (sumtop + 1); k++)
             {
                 cosine_k = cos((2*PI*k)/n);
@@ -173,7 +197,7 @@ int main()
             
             
             //summing the terms to calculate the scale factor r sub n
-            double scale_factor = 1/( 2*(1 + sum_total));
+            scale_factor = 1/( 2*(1 + sum_total));
 
             //Calculating the approximate radius of the polygon
             for (int i = 0; i < vertices.size(); i++)
@@ -231,10 +255,13 @@ int main()
                         false_poly_mid_x = (false_largest_x + false_smallest_x)/2;
                         }
                 }
+
+                //Normal point calculation
                 vert_select = (rand() % vertices.size());
                 midpoint_x = (vertices[vert_select].x + points[points.size()-1].x)*scale_factor;
                 midpoint_y = (vertices[vert_select].y + points[points.size()-1].y)*scale_factor;
 
+                //Special case point calculation for pentagons
                 if(vertices.size() == 5)
                 {
                     midpoint_x = -(vertices[vert_select].x - points[points.size()-1].x) / 8 * 3 + vertices[vert_select].x;
@@ -276,6 +303,7 @@ int main()
             window.draw(instruction_text);
         }
 
+        //Drawing vertices
         for(int i = 0; i < vertices.size(); i++)
         {
             RectangleShape rect(Vector2f(2,2));
@@ -283,6 +311,7 @@ int main()
             rect.setFillColor(Color::Red);
             window.draw(rect);
         }
+        //Drawing points for shapes past the pentagon which require a unique translation
         if(vertices.size() > 5)
         {
             for(int i = 20; i < points.size(); i++)
@@ -293,6 +322,7 @@ int main()
                 window.draw(rectp);
             }
         }
+        //Normal drawing points for shapes of 5 sides and below
         else
         {
             for(int i = 20; i < points.size(); i++)
